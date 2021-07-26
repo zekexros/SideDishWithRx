6932e52e939be7a4b900d469048295c7161729ac
zeke <yangsin1031@naver.com>
2021-07-26 14:55:33 +0900

    🔧 프로젝트 초기화

---
+++ SideDishWithRx/SideDishWithRx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideDishWithRx
+//
+//  Created by 양준혁 on 2021/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
