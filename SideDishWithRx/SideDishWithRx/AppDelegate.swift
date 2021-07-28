394d8f8d99a1fe0a3d2053f84d42b8a666ad368f
zeke <yangsin1031@naver.com>
2021-07-28 14:54:39 +0900

    🔧 iOS13.0을 기준으로 window초기화 분기처리

--- SideDishWithRx/SideDishWithRx/AppDelegate.swift
+++ SideDishWithRx/SideDishWithRx/AppDelegate.swift
@@ -9,11 +9,19 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if #available(iOS 13, *) {
+            
+        } else {
+//            window = UIWindow()
+//            let rootVc = MainViewController()
+//            window?.rootViewController = rootVc
+//            window?.makeKeyAndVisible()
+        }
+        
         return true
     }
 
