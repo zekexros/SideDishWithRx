8ba2dfa75773fc5ba2484566e5528f34620ea254
zeke <yangsin1031@naver.com>
2021-08-03 10:19:57 +0900

    ⚙ tableview cell 임시 구현 (#2)

---
+++ SideDishWithRx/SideDishWithRx/View/DishTableViewCell.swift
@@ -0,0 +1,55 @@
+//
+//  DishTableViewCell.swift
+//  SideDishWithRx
+//
+//  Created by 양준혁 on 2021/08/02.
+//
+
+import UIKit
+import SnapKit
+
+class DishTableViewCell: UITableViewCell {
+    
+    static let cellID = "DishTableViewCell"
+    let dishPhotography = UIImageView()
+    var title = UILabel()
+    let dishDescription = UILabel()
+    let nPrice = UILabel()
+    let sPrice = UILabel()
+    let badge = UIView()
+    let stackView = UIStackView()
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.addSubviews()
+        self.autoLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
+        addSubview(title)
+    }
+}
+
+extension DishTableViewCell {
+    func autoLayout() {
+        title.snp.makeConstraints { title in
+            title.edges.equalToSuperview()
+        }
+    }
+}
