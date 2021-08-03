429ce7046dc4b019775a521c15b477a9208a0866
zeke <yangsin1031@naver.com>
2021-08-03 10:18:09 +0900

    🔧 dish data들을 fetch하는 메소드 구현 (#2)

--- SideDishWithRx/SideDishWithRx/ViewModel/MainViewModel.swift
+++ SideDishWithRx/SideDishWithRx/ViewModel/MainViewModel.swift
@@ -9,11 +9,24 @@ import Foundation
 import RxSwift
 import RxCocoa
 import Action
+import RxDataSources
 
 class MainViewModel: CommonViewModel {
-    var mainDishList: [Dish]?
-    var soupList : [Dish]?
-    var sideDishLish: [Dish]?
+    var mainDishList = BehaviorSubject<[Dish]>(value: [])
+    var soupList = BehaviorSubject<[Dish]>(value: [])
+    var sideDishList = BehaviorSubject<[Dish]>(value: [])
+    var sections = BehaviorRelay<[SectionOfCustomData]>(value: [])
+    let dataSource = RxTableViewSectionedReloadDataSource<SectionOfCustomData> { dataSource, tableView, indexPath, item in
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "DishTableViewCell", for: indexPath) as? DishTableViewCell else { return UITableViewCell() }
+        cell.title.text = item.description
+        return cell
+    }
+    var apiService: APIType
+    
+    init(sceneCoordinator: SceneCoordinatorType, apiService: APIType) {
+        self.apiService = apiService
+        super.init(sceneCoordinator: sceneCoordinator)
+    }
     
     func detailAction() -> Action<Dish, Void> {
         return Action { dish in
@@ -22,5 +35,30 @@ class MainViewModel: CommonViewModel {
             
             return self.sceneCoordinator.transition(to: detailScene, using: .push, animated: true).asObservable().map{ _ in }
         }
+    }
+    
+    func fetchDishes() {
+        apiService.fetchDish(path: .mainDish)
+            .subscribe(mainDishList)
+            .disposed(by: rx.disposeBag)
+        
+        apiService.fetchDish(path: .sideDish)
+            .subscribe(sideDishList)
+            .disposed(by: rx.disposeBag)
+        
+        apiService.fetchDish(path: .soup)
+            .subscribe(soupList)
+            .disposed(by: rx.disposeBag)
+        
+        Observable.combineLatest([mainDishList, sideDishList, soupList], resultSelector: { dishes in
+            let dishes = [
+                SectionOfCustomData(header: "메인", items: dishes[0]),
+                SectionOfCustomData(header: "반찬", items: dishes[1]),
+                SectionOfCustomData(header: "스프", items: dishes[2])
+            ]
+            self.sections.accept(dishes)
+        })
+        .subscribe()
+        .disposed(by: rx.disposeBag)
     }
 }
