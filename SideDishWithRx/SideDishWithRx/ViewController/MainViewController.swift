4866d17aa876681042d62a26e2622bf4d4b78bfc
zeke <yangsin1031@naver.com>
2021-08-03 14:58:48 +0900

    🔧 cell과 section의 height설정 (#3)

--- SideDishWithRx/SideDishWithRx/ViewController/MainViewController.swift
+++ SideDishWithRx/SideDishWithRx/ViewController/MainViewController.swift
@@ -21,7 +21,7 @@ class MainViewController: UIViewController, ViewModelBindableType {
         self.view.addSubview(tableView)
         return tableView
     }()
-
+    
     func configureDataSource(_ dataSource: RxTableViewSectionedReloadDataSource<SectionOfCustomData>) {
         dataSource.titleForHeaderInSection = { dataSource, indexPath in
             return dataSource.sectionModels[indexPath].header
@@ -36,6 +36,7 @@ class MainViewController: UIViewController, ViewModelBindableType {
         configureAutoLayout()
         viewModel.fetchDishes()
         configureDataSource(viewModel.dataSource)
+        mainDishListTableView.rx.setDelegate(self).disposed(by: rx.disposeBag)
     }
     
     func bindViewModel() {
@@ -51,6 +52,17 @@ class MainViewController: UIViewController, ViewModelBindableType {
     }
 }
 
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 32
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
+    }
+    
+    
+}
 
 //AutoLayout
 extension MainViewController {
