d3928d02e3f089a1afdf4cd7039d9d0c1e83741f
zeke <yangsin1031@naver.com>
2021-08-14 00:54:20 +0900

    🔧 image fetch하고 바인딩 (#11)

--- SideDishWithRx/SideDishWithRx/ViewController/DetailViewController.swift
+++ SideDishWithRx/SideDishWithRx/ViewController/DetailViewController.swift
@@ -15,16 +15,7 @@ class DetailViewController: UIViewController, ViewModelBindableType {
 
     var viewModel: DetailViewModel!
     
-    let detailScrollView: UIScrollView = {
-        var scrollView = UIScrollView()
-        scrollView.isPagingEnabled = true
-        return scrollView
-    }()
-    
-    let imagesScrollView: UIScrollView = {
-        var scrollView = UIScrollView()
-        return scrollView
-    }()
+    let detailScrollView = DetailScrollView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,7 +31,79 @@ class DetailViewController: UIViewController, ViewModelBindableType {
         
         viewModel.fetchDetailDish()
         
+        Observable<String>.just(viewModel.dish.title)
+            .asDriver(onErrorJustReturn: "")
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        viewModel.fetchImages()
+            .observe(on: MainScheduler.instance)
+            .map { UIImage(data: $0) }
+            .map { image -> UIImageView in
+                let imageView = UIImageView(image: image)
+                imageView.contentMode = .scaleAspectFit
+                return imageView
+            }
+            .enumerated()
+            .do { [unowned self] (index, imageView) in
+                let xPosition = self.view.frame.width * CGFloat(index)
+                let bounds = self.detailScrollView.imagesScrollView.bounds
+                imageView.frame = CGRect(x: xPosition, y: 0, width: bounds.width, height: bounds.height)
+                self.detailScrollView.imagesScrollView.contentSize.width = self.view.frame.width * CGFloat(index+1)
+                self.detailScrollView.configureImagesScrollView(view: imageView)
+            }
+            .subscribe()
+            .disposed(by: rx.disposeBag)
+            
+        detailScrollView.setUpDishInformationStackView(title: viewModel.dish.title, description: viewModel.dish.description, nPrice: viewModel.dish.nPrice, sPrice: viewModel.dish.sPrice, badge: viewModel.dish.badge)
+        
+        viewModel.detailDish
+            .map { ($0.data.point, $0.data.deliveryInfo, $0.data.deliveryFee) }
+            .asDriver(onErrorJustReturn: ("", "", ""))
+            .drive { [unowned self] (point, deliveryInfo, deliveryFee) in
+                self.detailScrollView.configureDetailInformationStackView(point: point, info: deliveryInfo, fee: deliveryFee)
+            }
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.quantity
+            .asDriver()
+            .map { String($0) }
+            .drive(detailScrollView.quantityStackView.quantityLabel.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        detailScrollView.quantityStackView.upButton.rx.tap
+            .map { [unowned self] _ in
+                self.viewModel.quantity.value + 1
+            }
+            .bind(to: viewModel.quantity)
+            .disposed(by: rx.disposeBag)
+        
+        detailScrollView.quantityStackView.downButton.rx.tap
+            .map { [unowned self] _ -> Int in
+                let value = self.viewModel.quantity.value - 1
+                return value < 1 ? 1 : value
+            }
+            .bind(to: viewModel.quantity)
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.detailDish
+            .map { $0.data.prices[0] }
+            .asDriver(onErrorJustReturn: "")
+            .drive(detailScrollView.priceLabel.rx.text)
+            .disposed(by: rx.disposeBag)
+            
+        viewModel.fetchDetailImages()
+            .observe(on: MainScheduler.instance)
+            .map { UIImage(data: $0) }
+            .map { image -> UIImageView in
+                let imageView = UIImageView(image: image)
+                imageView.contentMode = .scaleAspectFit
+                return imageView
+            }
+            .bind(onNext: { imageView in
+                self.detailScrollView.detailSectionStackView.addArrangedSubview(imageView)
+            })
+            .disposed(by: rx.disposeBag)
     }
     
     override func viewWillAppear(_ animated: Bool) {
--- SideDishWithRx/SideDishWithRx/ViewModel/DetailViewModel.swift
+++ SideDishWithRx/SideDishWithRx/ViewModel/DetailViewModel.swift
@@ -14,6 +14,7 @@ class DetailViewModel: CommonViewModel {
     
     let dish: Dish
     let detailDish = PublishSubject<DetailDish>()
+    let quantity = BehaviorRelay<Int>(value: 1)
     
     init(sceneCoordinator: SceneCoordinatorType, repository: RepositoryType, model: Dish) {
         self.dish = model
@@ -24,6 +25,20 @@ class DetailViewModel: CommonViewModel {
         repository.fetch(path: EndPoint(path: .detail), id: dish.detailHash, decodingType: DetailDish.self)
             .subscribe(detailDish)
             .disposed(by: rx.disposeBag)
+    }
+
+    func fetchImages() -> Observable<Data> {
+        return detailDish.asObservable()
+            .flatMap { Observable.from($0.data.thumbImages) }
+            .compactMap{ URL(string: $0) }
+            .flatMap { self.repository.fetch(url: $0) }
+    }
+    
+    func fetchDetailImages() -> Observable<Data> {
+        return detailDish.asObservable()
+            .flatMap { Observable.from($0.data.detailSection) }
+            .compactMap{ URL(string: $0) }
+            .flatMap { self.repository.fetch(url: $0) }
     }
     
     lazy var popAction = CocoaAction { [unowned self] in
